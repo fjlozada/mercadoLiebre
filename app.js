ce1a085dd8b12db5f4032372077cc5de0e985d27
fjlozada <fjlozada@hotmail.com>
2023-07-17 23:08:40 -0400

    segundo commit con cambios para render

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express()
-app.listen(3000, () => {console.log('Server en 3000 OK')});
+const port = process.env.PORT || 3001;
+app.listen(port, () => {console.log('Server en '+port+' OK')});
 app.use(express.static('public'));
 app.get('/',(req,res) => {res.sendFile(path.join(__dirname,'/views/home.html'))});
 
